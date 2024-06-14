967a3a000fe78e012c7b563d6eb01ba30849de7c
Naol Lemessa <lemesanaol93@gmail.com>
2024-06-14 15:57:17 +0300

    new additions

--- db_config/db.js
+++ db_config/db.js
@@ -1,30 +1,37 @@
+
 require("dotenv").config();
 const express = require("express");
+const mysql = require("mysql2/promise"); 
+const jwt=require("jsonwebtoken")
 const app = express();
-const port = process.env.PORT|| 7000;
+
+const port = process.env.PORT || 7000;
 const { DB_HOST, DB_USER, DB_PASSWORD, DATABASE } = process.env;
 
-const mysql = require("mysql");
-
-const db = mysql.createPool({
+const database = mysql.createPool({
     host: "localhost",
-    user:"root",
+   user:"root",
     password:"",
     database:"evanghadiforum",
-});
 
-db.getConnection((err) => {
-    if (err) {
-        console.log("Database connection error: ", err.message);
-    } else {
-        console.log("Connection to database established successfully");
-    }
+
 });
+(async () => {
+    try {
+        const connection = await database.getConnection();
+        console.log("Connection to database established successfully");
+        connection.release(); 
+    } catch (err) {
+        console.log("Database connection error: ", err.message);
+    }
+})();
 
 app.listen(port, (err) => {
     if (err) {
         console.log("Server error: ", err.message);
     } else {
-        console.log(`seServer is running correctly on port ${port}`);
+        console.log(`Server is running correctly on port ${port}`);
     }
 });
+
+module.exports = database;
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express=require("express")
 const app=new express()
-const port= process.env.port || 4321
+const port= process.env.port || 6214
 const user=require("./routes/user")
-
+app.use(express.json())
 app.use('/api/user',user)
 app.listen(port,(err)=>{
     if(err){
